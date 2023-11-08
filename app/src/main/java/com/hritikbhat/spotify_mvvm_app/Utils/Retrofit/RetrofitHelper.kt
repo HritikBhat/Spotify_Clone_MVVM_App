f247927afd084f5e7fda4ce6d7c05b23873f6277
HritikBhat <43090040+HritikBhat@users.noreply.github.com>
2023-11-08 19:24:36 +0530

    Update RetrofitHelper.kt

--- app/src/main/java/com/hritikbhat/spotify_mvvm_app/Utils/Retrofit/RetrofitHelper.kt
+++ app/src/main/java/com/hritikbhat/spotify_mvvm_app/Utils/Retrofit/RetrofitHelper.kt
@@ -4,8 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
-    //const val BASE_URL = "http://192.168.1.104/HDB/spotify_lyte/" // Replace with your actual base URL
-    const val BASE_URL = "https://www.pentago.in/HDB/spotify_lyte/"
+    const val BASE_URL = ""
 
     fun getInstance() : Retrofit{
         return Retrofit.Builder()
